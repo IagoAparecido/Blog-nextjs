c10cdc21781b8873e70740818d66fcfe25d7b547
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-08-18 15:05:15 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import MyProfilePic from "./components/MyProfilePic";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Iago's Blog",
+  description: "Created by Dave Gray",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} dark:bg-slate-800`}>
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  );
+}
